cc1b92783b37ca334bd02fbc544aaf9966b4e4b6
Vlad <vladfacepalm@gmail.com>
2020-10-17 17:58:04 +0300

    add 4 pages

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,31 @@
 $(function () {
 
   $('.burger-content').on('click', function () {
-      $('.header__menu').addClass('active');
-      $('body').addClass('overflowHidden');
+    $('.header__menu').addClass('active');
+    $('body').addClass('overflowHidden');
   });
 
   $('.mobile-menu-close').on('click', function () {
-      $('.header__menu').removeClass('active');
-      $('body').removeClass('overflowHidden');
+    $('.header__menu').removeClass('active');
+    $('body').removeClass('overflowHidden');
+  });
+
+
+  $(".tabs-js").on("click", "div:not(.active)", function(e) {
+    $(this)
+
+    .addClass("active")
+    .siblings()
+    .removeClass("active")
+    .parents(".tabs-info-js")
+    .find(".team-content__tabs-wrap-content")
+    .children()
+    .hide()
+    .eq($(this).index())
+    .fadeIn(300);
+
+    e.preventDefault();
+
   });
 
 
@@ -24,7 +42,9 @@ $(function () {
   pageWidget([
       ['index', 'Homepage'],
       ['about', 'About'],
-      ['team', 'Team']
+      ['team', 'Team'],
+      ['single-team', 'Team Single'],
+      ['practices', 'Practices'],
   ]);
   
 });
